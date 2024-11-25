ec9d9e42e08d728619f14103ebe6ac9919c893e1
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-11-25 16:23:46 +0200

    Initial commit: generate project from template

---
+++ src/language/stella-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { StellaAstType, Person } from './generated/ast.js';
+import type { StellaServices } from './stella-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: StellaServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.StellaValidator;
+    const checks: ValidationChecks<StellaAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class StellaValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
