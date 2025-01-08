82fa0966d64960a0fd8931450fc0ed306fd7c64c
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2025-01-09 01:24:30 +0200

    Wrap the main script in an async function
    Top-level await was messing up something. Probably a bug in Chrome

--- src/web/main.ts
+++ src/web/main.ts
@@ -6,45 +6,49 @@ import { KeyMod, KeyCode } from "monaco-editor";
 
 hideHeaderInIframe();
 
-// Initialize the Monaco editor
-const monacoRoot = document.getElementById("monaco-editor-root")!;
-const monacoWrapper = await executeExtended(monacoRoot);
+async function main() {
+  // Initialize the Monaco editor
+  const monacoRoot = document.getElementById("monaco-editor-root")!;
+  const monacoWrapper = await executeExtended(monacoRoot);
 
-const typecheckButton = document.getElementById(
-  "typecheck"
-) as HTMLButtonElement;
-const compileButton = document.getElementById("compile") as HTMLButtonElement;
-const resultElement = document.getElementById("result") as HTMLDivElement;
+  const typecheckButton = document.getElementById(
+    "typecheck"
+  ) as HTMLButtonElement;
+  const compileButton = document.getElementById("compile") as HTMLButtonElement;
+  const resultElement = document.getElementById("result") as HTMLDivElement;
 
-function typecheck() {
-  resultElement.innerText = "Typechecking...";
+  function typecheck() {
+    resultElement.innerText = "Typechecking...";
 
-  const code = monacoWrapper.getEditor()?.getValue() ?? "";
-  const { result, status } = stella.typecheck(code);
+    const code = monacoWrapper.getEditor()?.getValue() ?? "";
+    const { result, status } = stella.typecheck(code);
 
-  resultElement.innerText = result;
+    resultElement.innerText = result;
+  }
+
+  function compile() {
+    resultElement.innerText = "Compiling...";
+
+    const code = monacoWrapper.getEditor()?.getValue() ?? "";
+    const { result, status } = stella.compile(code);
+
+    resultElement.innerText = result;
+  }
+
+  // Show correct keybinding based on platform
+  const prefix = navigator.platform.includes("Mac") ? "⌘" : "^";
+  typecheckButton.innerText = `Typecheck (${prefix} + Enter)`;
+
+  // Bind the typecheck and compile functions to the buttons
+  monacoWrapper
+    .getEditor()
+    ?.addCommand(KeyMod.CtrlCmd + KeyCode.Enter, typecheck);
+
+  typecheckButton.addEventListener("click", typecheck);
+  compileButton.addEventListener("click", compile);
+
+  // Initial typecheck
+  typecheck();
 }
 
-function compile() {
-  resultElement.innerText = "Compiling...";
-
-  const code = monacoWrapper.getEditor()?.getValue() ?? "";
-  const { result, status } = stella.compile(code);
-
-  resultElement.innerText = result;
-}
-
-// Show correct keybinding based on platform
-const prefix = navigator.platform.includes("Mac") ? "⌘" : "^";
-typecheckButton.innerText = `Typecheck (${prefix} + Enter)`;
-
-// Bind the typecheck and compile functions to the buttons
-monacoWrapper
-  .getEditor()
-  ?.addCommand(KeyMod.CtrlCmd + KeyCode.Enter, typecheck);
-
-typecheckButton.addEventListener("click", typecheck);
-compileButton.addEventListener("click", compile);
-
-// Initial typecheck
-typecheck();
+main();
